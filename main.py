22dd88698c4c605562d5de9a5481569b2a12b455
aburtasenkov <aburtasenkov@gmail.com>
2025-02-02 14:40:35 +0100

    Blocks now are checked for collisions with existing blocks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import typing
 from block import *
 
 def get_fps(clock: pygame.time.Clock) -> int:
@@ -11,21 +12,44 @@ def make_game_area(screen: pygame.Surface) -> Stationary_Block:
     return game_area
 
 def pos_within_rect(rect: pygame.Rect, pos: tuple[int, int]) -> bool:
-    """return true coordinate pos is within rects edges"""
+    """return true coordinate pos is within rect's edges"""
     return ((rect.topleft[0] <= pos[0] and pos[0] <= rect.topright[0]) and 
             (rect.topleft[1] <= pos[1] and pos[1] <= rect.bottomright[1]))
 
-def move_in_rect(rect: pygame.Rect, block: Moving_Block, offset: tuple[int, int]) -> Moving_Block:
+def check_collisions(game_area: pygame.Rect, stationary_blocks: typing.Iterable[Stationary_Block], pos: tuple[int,int]) -> bool:
     """
-    move block if its center coordinate stays within rects edges
+    check for collisions with game_area and all stationary blocks from block_list
 
-    do it by pre-moving and checking if block.rect.center coordinate is within rect
+    requirements:
+    1. collision with game_area must be true -> within game_area
+    2. all collisions with block_list objects must be false -> center position is not within any existing blocks
+
+    return value:
+        true if both requirements are true
+        false if at least one of requirements is not met
+    """
+
+    if (not pos_within_rect(game_area, pos)):
+        return False
+    
+    for block in stationary_blocks:
+        if (pos_within_rect(block.outline, pos)):
+            return False
+
+    return True
+
+def check_collisions_move(game_area: pygame.Rect, stationary_blocks: typing.Iterable[Stationary_Block], block: Moving_Block, offset: tuple[int, int]) -> Moving_Block:
+    """
+    move block if check_collisions returns true
+
+    do it by pre-moving and checking if block.outline.center coordinate is within game_area and outside of blocks in stationary_blocks 
         if True - return block
         else move block back
     """
 
     block = block.move(offset)
-    if not pos_within_rect(rect, block.outline.center):
+
+    if not check_collisions(game_area, stationary_blocks, block.outline.center):
         block = block.move((-offset[0], -offset[1]))
 
     return block
@@ -44,7 +68,7 @@ def make_moving_timer():
 
     return time_passed
 
-stationary_blocks = []
+stationary_blocks: list[Stationary_Block] = []
 
 def main():
     pygame.init()
@@ -67,34 +91,35 @@ def main():
                 if event.key == pygame.K_UP:
                     continue
                 elif event.key == pygame.K_DOWN:
-                    current_block = move_in_rect(game_area.outline, current_block, (0, MOVE_OFFSET))
+                    current_block = check_collisions_move(game_area.outline, stationary_blocks, current_block, (0, MOVE_OFFSET))
                 elif event.key == pygame.K_LEFT:
-                    current_block = move_in_rect(game_area.outline, current_block, (-MOVE_OFFSET, 0))
+                    current_block = check_collisions_move(game_area.outline, stationary_blocks, current_block, (-MOVE_OFFSET, 0))
                 elif event.key == pygame.K_RIGHT:
-                    current_block = move_in_rect(game_area.outline, current_block, (MOVE_OFFSET, 0))
+                    current_block = check_collisions_move(game_area.outline, stationary_blocks, current_block, (MOVE_OFFSET, 0))
                 
         screen.fill(black)
 
         if (time_passed(TIME_UNTIL_DECREASE)):
             last_pos = current_block.outline.center
-            block = move_in_rect(game_area.outline, current_block, (0, MOVE_OFFSET))
+
+            current_block = check_collisions_move(game_area.outline, stationary_blocks, current_block, (0, MOVE_OFFSET))
+
             # Block has not moved -> make it stationary (base class)
             if (current_block.outline.center == last_pos):
-                print(current_block.outline.center, last_pos)
 
-                block.__class__ = block.__class__.__base__
-                stationary_blocks.append(stationary_blocks)
+                current_block.__class__ = current_block.__class__.__base__
+                stationary_blocks.append(current_block)
 
-                current_block = Moving_Block(START_POS, BLOCK_SIZE)
-
-        for block in stationary_blocks:
-            print("Is within blocks from block_list", pos_within_rect(block.outline, current_block.outline.center))
+        # create new block for moving if it has sunk
+        if (type(current_block) == Stationary_Block):
+            current_block = Moving_Block(START_POS, BLOCK_SIZE)
 
         game_area.draw(screen)
         current_block.draw(screen)
+        
         for block in stationary_blocks:
             block.draw(screen)
-            
+
         pygame.display.flip()
 
 
