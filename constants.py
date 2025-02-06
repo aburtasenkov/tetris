3b3a941a5ed4bee65c62b0c1fe92aba2a53276a9
aburtasenkov <aburtasenkov@gmail.com>
2025-02-06 15:39:12 +0100

    Moved starting point 1 square down

--- constants.py
+++ constants.py
@@ -20,7 +20,7 @@ MOVE_OFFSET = BLOCK_SIZE[0]
 
 # top left corners of game area and starting position for each block
 GAME_AREA_POS = (50, 50)
-START_POS = (GAME_AREA_POS[0] + (GAME_AREA_BLOCKS_HORIZONTAL / 2 * BLOCK_WIDTH), GAME_AREA_POS[1] + BLOCK_SIZE[1])  # middle of game_area and top coordinate
+START_POS = (GAME_AREA_POS[0] + (GAME_AREA_BLOCKS_HORIZONTAL / 2 * BLOCK_WIDTH), GAME_AREA_POS[1] + BLOCK_SIZE[1] * 2)  # middle of game_area and top coordinate
 
 # time in ms that it takes a block to move 1 square down
 TIME_UNTIL_DECREASE = 1000
